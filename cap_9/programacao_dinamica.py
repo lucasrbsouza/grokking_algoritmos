1b7d1cb116c57094c071110ef4d15209ff4ac747
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2025-05-07 16:04:28 -0300

    codigo da mochila programcao dinamica

--- cap_9/programacao_dinamica.py
+++ cap_9/programacao_dinamica.py
@@ -1,1 +1,14 @@
-"""criando a solucao para um probelma de substring em comum usanod programacao dinamica"""
+"""criando a solucao para um probelma da mochila em comum usanod programacao dinamica"""
+# Exemplo em Python
+def mochila(valores, pesos, capacidade):
+    n = len(valores)
+    tabela = [[0] * (capacidade + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, capacidade + 1):
+            if pesos[i - 1] <= w:
+                tabela[i][w] = max(valores[i - 1] + tabela[i - 1][w - pesos[i - 1]], tabela[i - 1][w])
+            else:
+                tabela[i][w] = tabela[i - 1][w]
+
+    return tabela[n][capacidade]
